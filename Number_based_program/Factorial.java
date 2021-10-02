17c1ad7704e66da5e329fc84cb811ea03d493205
Subham Kumar <69760328+subham500071430@users.noreply.github.com>
2021-10-02 15:46:11 +0530

    Update Factorial.java

--- Number_based_program/Factorial.java
+++ Number_based_program/Factorial.java
@@ -1,13 +1,15 @@
 /* Factorial Eg: 5!=5*4*3*2*1  */
 
 import java.util.*;
+import java.io.*;
+
 class Main 
 {
     public static void main(String[] args)
     {
-       Scanner sc=new Scanner(System.in);
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        int n,i,fact=0;
-       n=sc.nextInt();
+       n = Integer.parseInt(br.readLine()); // Fast IO
        
        for(i=n-1;i>1;i--)
        {
